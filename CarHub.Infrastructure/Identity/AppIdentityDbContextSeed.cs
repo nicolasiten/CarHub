2ad477adda5627754384e21e94651f4a7cf873c2
Nicolas Iten <nici.iten@gmail.com>
2019-11-10 11:15:22 +0100

    Identity Seed

---
+++ CarHub.Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHub.Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        private const string AdminUser = "admin@carhub.com";
+
+        public static async Task SeedAsync(UserManager<IdentityUser> userManager)
+        {
+            if ((await userManager.FindByEmailAsync("admin@carhub.com")) == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    Email = AdminUser,
+                    UserName = AdminUser
+                };
+
+                await userManager.CreateAsync(user, "Test1234!");
+            }
+        }
+    }
+}
--- CarHub.Web/Program.cs
+++ CarHub.Web/Program.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CarHub.Infrastructure.Identity;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CarHub.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                // identity Db Setup
+                var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+                await identityContext.Database.MigrateAsync();
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                await AppIdentityDbContextSeed.SeedAsync(userManager);
+            }
+
+            await host.RunAsync();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
